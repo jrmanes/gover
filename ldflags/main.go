30396326b9f8d795f865ecf41b54affacd29c9f6
Jose Ramon Mañes <jr.mb47@gmail.com>
2020-10-29 22:10:35 +0100

    first commit

--- ldflags/main.go
+++ ldflags/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 var version = "development"
 
 func main() {
+	fmt.Printf("production-%s", time.Now())
 	fmt.Printf("Version %s\n", version)
 }
-  
