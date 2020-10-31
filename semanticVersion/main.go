628f44cea0bd03c5ed39de613925e6c67051a681
Jose Ramon Mañes <jr.mb47@gmail.com>
2020-10-31 23:18:48 +0100

    added semantic version

---
+++ semanticVersion/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "strings"
+    "strconv"
+    "io/ioutil"
+
+    "gopkg.in/yaml.v2"
+)
+
+type Config struct {
+        Version string `yaml:"version"`
+        Language string `yaml:"language"`
+}
+
+type SemanticVersion struct {
+	Major int 
+	Minor int
+	Patch int
+}
+
+func (v *Config)getConf() *Config {
+	defaultFileName := "gover.yml"
+        yamlFile, err := ioutil.ReadFile(defaultFileName)
+        if err != nil {
+                log.Println("Error:", err)
+        }
+	err = yaml.Unmarshal(yamlFile, v)
+	if err != nil {
+		log.Fatalf("Cannot Unmarshal file: %v", err)
+	}
+	return v
+}
+
+func (v *Config)GetVersion() SemanticVersion {
+	var major, minor, patch int
+	s := strings.Split(v.Version, ".")
+
+//	fmt.Println(s)
+	fmt.Printf("Current version: %s\n", v.Version)
+	
+	major, _ = strconv.Atoi(s[0])
+	minor, _ = strconv.Atoi(s[1])
+	patch, _ = strconv.Atoi(s[2])
+	
+	patch += 1
+
+	sm := SemanticVersion{
+		major,
+		minor,
+		patch,
+	}
+	
+	return sm
+}
+
+
+func main() {
+	var v Config
+	var sm SemanticVersion
+
+	v.getConf()
+//	fmt.Printf("Version: %s - Language: %s", v.Version, v.Language)
+
+	sm = v.GetVersion()
+	fmt.Println("Next Version: ", sm.Major)
+	fmt.Println("Next Version: ", sm.Minor)
+	fmt.Println("Next Version: ", sm.Patch)
+}
